06e50a03b5bba3674bdbae29943f3962e0b22e06
Ben Neale <ben@atticus7.co.uk>
2023-10-25 09:00:12 +0100

    Test plugin_dir input

--- actions/collect-plugins/index.js
+++ actions/collect-plugins/index.js
@@ -1,7 +1,9 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fs = require("fs");
 
-console.log(github);
+const plugins_dir = core.getInput("plugins_dir");
+console.log(plugins_dir);
 
 // try {
 //   // `who-to-greet` input defined in action metadata file
@@ -15,3 +17,63 @@ console.log(github);
 // } catch (error) {
 //   core.setFailed(error.message);
 // }
+/*
+try {
+  const plugins_dir = "html/content/plugins";
+  const premium_plugins_dir = "premium-plugins";
+  const composer_json_file = "html/composer.json";
+  // get the directory contents
+  const plugins = fs.readdirSync(plugins_dir);
+  const premium_plugins = fs
+    .readdirSync(premium_plugins_dir)
+    .map((x) => x.replace(".zip", ""));
+  //console.log(premium_plugins);
+  const exclude = [".DS_Store", "index.php"];
+
+  for (let i = 0; i < exclude.length; i++) {
+    let index = plugins.indexOf(exclude[i]);
+    if (index > -1) {
+      // only splice array when item is found
+      plugins.splice(index, 1); // 2nd parameter means remove one item only
+    }
+  }
+
+  const composer_json = fs.readFileSync(composer_json_file);
+  const composer_plugins = Object.getOwnPropertyNames(
+    JSON.parse(composer_json)["require"]
+  ).map((x) => x.split("/")[1]);
+  //console.log(composer_plugins);
+
+  //console.log(files.length);
+
+  let type = "";
+
+  for (let i = 0; i < plugins.length; i++) {
+    if (plugins[i].startsWith("a7-")) {
+      type = "custom";
+    } else if (premium_plugins.includes(plugins[i])) {
+      type = "premium";
+    } else if (composer_plugins.includes(plugins[i])) {
+      type = "composer";
+    } else {
+      type = "free";
+    }
+    plugins[i] = {
+      name: plugins[i],
+      type: type,
+    };
+  }
+
+  json = JSON.stringify(plugins);
+  //console.log(json);
+
+  fs.writeFile("plugins.json", json, (err) => {
+    // Checking for errors
+    if (err) throw err;
+
+    console.log("Done writing"); // Success
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
+*/
