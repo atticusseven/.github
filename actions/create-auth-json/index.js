fca532425e3cb6cdde7b23d06486255a9e64a16a
Ben Neale <ben@atticus7.co.uk>
2023-10-30 13:03:09 +0000

    Multiple Actions - should commit more often!

---
+++ actions/create-auth-json/index.js
@@ -0,0 +1,32 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs");
+
+try {
+  const workspace = core.getInput("workspace");
+  const auth_json_filename = workspace + "/" + core.getInput("json_filename");
+
+  const auth_json = {
+    "http-basic": {
+      "connect.advancedcustomfields.com": {
+        username: core.getInput("acf_licence_key"),
+        password: core.getInput("site_url"),
+      },
+    },
+    bearer: {
+      "composer.admincolumns.com": core.getInput("ac_token"),
+    },
+  };
+
+  auth_json = JSON.stringify(auth_json);
+  //console.log(json);
+
+  fs.writeFile(auth_json_filename, auth_json, (err) => {
+    // Checking for errors
+    if (err) throw err;
+
+    console.log("Done writing"); // Success
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
