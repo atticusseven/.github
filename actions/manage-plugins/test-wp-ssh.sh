d01439e85460c0996cc7aa7cecd7fd0f847630b9
Ben Neale <ben@atticus7.co.uk>
2023-10-31 15:45:24 +0000

    wp --info

--- actions/manage-plugins/test-wp-ssh.sh
+++ actions/manage-plugins/test-wp-ssh.sh
@@ -7,7 +7,9 @@ SSH_DESTINATION_DIR=$4
 
 wp_cli_default_params="--skip-plugins --skip-themes --ssh=${SSH_USER}@${SSH_HOST}:${SSH_DESTINATION_DIR}/html/wp"
 
-wp plugin list "${wp_cli_default_params}"
+
+wp --info
+#wp plugin list "${wp_cli_default_params}"
 
 
 
