bd736b7f292eaaeb113befae8c395a4791a260de
Ben Neale <ben@atticus7.co.uk>
2023-10-31 17:51:37 +0000

    use ssh-config in action

--- actions/manage-plugins/test-wp-ssh.sh
+++ actions/manage-plugins/test-wp-ssh.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 PLUGINS_JSON_FILE=$1
-SSH_USER=$2
-SSH_HOST=$3
-SSH_DESTINATION_DIR=$4
+SSH_DESTINATION_DIR=$2
 
-wp_cli_default_params="--skip-plugins --skip-themes --ssh=${SSH_USER}@${SSH_HOST}:${SSH_DESTINATION_DIR}/html/wp"
+wp_cli_default_params="--skip-plugins --skip-themes --ssh=server:${SSH_DESTINATION_DIR}/html/wp"
 
 echo "${wp_cli_default_params}"
 #wp --info
