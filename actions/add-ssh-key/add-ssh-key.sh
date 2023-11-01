34116b936cd7a3fb986653d9b0c7c6fb4def226d
Ben Neale <ben@atticus7.co.uk>
2023-11-01 10:42:34 +0000

    Remove line from private key

--- actions/add-ssh-key/add-ssh-key.sh
+++ actions/add-ssh-key/add-ssh-key.sh
@@ -7,7 +7,7 @@ SSH_PORT=$4
 #ADD PRIVATE KEY
 mkdir -p /home/runner/.ssh
 ssh-keyscan -p "${SSH_PORT}" "${SSH_HOST}" >> /home/runner/.ssh/known_hosts
-echo "${SSH_KEY}\r" > /home/runner/.ssh/github_actions
+echo "${SSH_KEY}" > /home/runner/.ssh/github_actions
 chmod 600 /home/runner/.ssh/github_actions
 eval "$(ssh-agent -a /tmp/ssh_agent.sock)"
 ssh-add /home/runner/.ssh/github_actions
