68f87f51349f9c3fb6205645c5860d03fcf9af73
Ben Neale <ben@atticus7.co.uk>
2023-11-03 10:35:56 +0000

    add conditional statements for get-site-url

--- actions/get-site-url/get-site-url.sh
+++ actions/get-site-url/get-site-url.sh
@@ -1,5 +1,13 @@
 DOMAIN=$1
 WP_ENV=$2
 
-site_url="${DOMAIN} ${WP_ENV}"
+
+if ["${WP_ENV}" = "dev"]; then
+    site_url="https://dev.${DOMAIN}"
+elif ["${WP_ENV}" = "staging"]; then
+    site_url="https://staging.${DOMAIN}"
+elif ["${WP_ENV}" = "production"]; then
+    site_url="https://www.${DOMAIN}"]
+fi
+
 echo "site-url=${site_url}"
