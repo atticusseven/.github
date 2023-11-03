44ff115522ae20441b83d4f24b47f4127f3e74f0
Ben Neale <ben@atticus7.co.uk>
2023-11-03 16:19:48 +0000

    Add env_redis_db; remove unnecessary inputs from setup

--- actions/dynamic-env-variables/dynamic-env-variables.sh
+++ actions/dynamic-env-variables/dynamic-env-variables.sh
@@ -3,7 +3,10 @@
 DOMAIN=$1
 WP_ENV=$2
 PROJECT=$3
+REDIS_DATABASES=$4
 
+redis_databases=`echo "${REDIS_DATABASES}" | json`
+env_redis_db=$(jq -r ".$WP_ENV" <<< "$redis_databases");
 
 if [[ "$WP_ENV" == "dev" ]]; then
     env_domain="dev.${DOMAIN}"
@@ -18,5 +21,6 @@ fi
 #mnt/falo/sites/dev/dev.findalondonoffice.co.uk
 env_path="/mnt/$PROJECT/sites/$WP_ENV/$env_domain"
 
-echo "env_domain=https://${env_domain}"
+echo "env_domain=https://${env_domain}"#
 echo "env_path=${env_path}"
+echo "env_redis_db=${env_redis_db}"
