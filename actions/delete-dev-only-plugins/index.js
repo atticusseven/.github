f0a376de2031f823053a03021895034f21698d3b
Ben Neale <ben@atticus7.co.uk>
2023-11-03 22:53:37 +0000

    Add delete-dev-only-plugins action

---
+++ actions/delete-dev-only-plugins/index.js
@@ -0,0 +1,29 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs");
+
+try {
+  const workspace = core.getInput("workspace");
+  const root_dir = core.getInput("root_dir");
+  const composer_json_file = workspace + "/" + root_dir + "/" + composer.json;
+  const composer_json = fs.readFileSync(composer_json_file);
+  const composer = JSON.parse(composer_json);
+
+  const dev_only_plugins_json = core.getInput("dev_only_plugins");
+  const dev_only_plugins = JSON.parse(dev_only_plugins_json);
+
+  for (let i = 0; i < dev_only_plugins.length; i++) {
+    delete composer.require["wpackagist-plugin/" + dev_only_plugins[i]];
+  }
+
+  json = JSON.stringify(composer);
+
+  fs.writeFile(composer_json_file, json, (err) => {
+    // Checking for errors
+    if (err) throw err;
+
+    console.log("Done writing"); // Success
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
