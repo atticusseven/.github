f015b1c564c35b59169a9828a41f684474a27640
Ben Neale <ben@atticus7.co.uk>
2023-11-06 15:36:34 +0000

    move switch-on-maintenance to .sh file to allow script to test for ,env existence before copying

---
+++ actions/deploy-via-rsync/switch-on-maintenance.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CONF_DIR=$1
+DESTINATION_PATH=$2
+
+if ssh server "test -e ${DESTINATION_PATH}/${CONF_DIR}/.env"; then
+    ssh server "mv ${DESTINATION_PATH}/${{inputs.conf_dir}}/.env ${DESTINATION_PATH}/${CONF_DIR}/old.env"
+    ssh server 'sed -e "s|A7_MAINTENANCE=\"off\"|A7_MAINTENANCE=\"on\"|" ${DESTINATION_PATH}/${CONF_DIR}/old.env > ${DESTINATION_PATH}/${CONF_DIR}/.env'
+fi
