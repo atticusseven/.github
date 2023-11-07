159bad8c4cec30d3f136f9b34f66e305db240eb0
Ben Neale <ben@atticus7.co.uk>
2023-11-07 14:24:15 +0000

    Double quotes in ssh server statement switch-on-maintenance.sh

--- actions/deploy-via-rsync/switch-on-maintenance.sh
+++ actions/deploy-via-rsync/switch-on-maintenance.sh
@@ -5,5 +5,5 @@ DESTINATION_PATH=$2
 
 if ssh server "test -e ${DESTINATION_PATH}/${CONF_DIR}/.env"; then
     ssh server "mv ${DESTINATION_PATH}/${CONF_DIR}/.env ${DESTINATION_PATH}/${CONF_DIR}/old.env"
-    ssh server 'sed -e "s|A7_MAINTENANCE=\"off\"|A7_MAINTENANCE=\"on\"|" ${DESTINATION_PATH}/${CONF_DIR}/old.env > ${DESTINATION_PATH}/${CONF_DIR}/.env'
+    ssh server "sed -e 's|A7_MAINTENANCE=\"off\"|A7_MAINTENANCE=\"on\"|' ${DESTINATION_PATH}/${CONF_DIR}/old.env > ${DESTINATION_PATH}/${CONF_DIR}/.env"
 fi
