90cb7f2bd633c9ce415ba8ab4318ae1532ce249b
Ben Neale <ben@atticus7.co.uk>
2024-01-26 13:46:50 +0000

    Debug: Output json strings and array in delete-dev-only-plugins

--- actions/delete-dev-only-plugins/index.js
+++ actions/delete-dev-only-plugins/index.js
@@ -7,10 +7,14 @@ try {
   const root_dir = core.getInput("root_dir");
   const composer_json_file = workspace + "/" + root_dir + "/composer.json";
   const composer_json = fs.readFileSync(composer_json_file);
+  console.log(composer_json);
   const composer = JSON.parse(composer_json);
+  console.log(composer);
 
   const dev_only_plugins_json = core.getInput("dev_only_plugins");
+  console.log(dev_only_plugins_json);
   const dev_only_plugins = JSON.parse(dev_only_plugins_json);
+  console.log(dev_only_plugins);
 
   for (let i = 0; i < dev_only_plugins.length; i++) {
     delete composer.require["wpackagist-plugin/" + dev_only_plugins[i]];
