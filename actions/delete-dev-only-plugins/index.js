4066021e3c77dbd9d1c57b92b569e94380664a04
Ben Neale <ben@atticus7.co.uk>
2024-01-26 14:20:31 +0000

    Remove Debugging

--- actions/delete-dev-only-plugins/index.js
+++ actions/delete-dev-only-plugins/index.js
@@ -6,16 +6,11 @@ try {
   const workspace = core.getInput("workspace");
   const root_dir = core.getInput("root_dir");
   const composer_json_file = workspace + "/" + root_dir + "/composer.json";
-  //const composer_json = fs.readFileSync(composer_json_file);
-  //console.log(composer_json);
-  //const composer = JSON.parse(composer_json);
-  //console.log(composer);
-  console.log("wibble");
+  const composer_json = fs.readFileSync(composer_json_file);
+  const composer = JSON.parse(composer_json);
+
   const dev_only_plugins_json = core.getInput("dev_only_plugins");
   const dev_only_plugins = JSON.parse(dev_only_plugins_json);
-  console.log(dev_only_plugins);
-
-  return;
 
   for (let i = 0; i < dev_only_plugins.length; i++) {
     delete composer.require["wpackagist-plugin/" + dev_only_plugins[i]];
